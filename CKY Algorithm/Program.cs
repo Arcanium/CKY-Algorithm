638d0d57e7c0f191f14ec6604eb5dcdc310ebf17
Jackson Montgomery <arcthelad56@yahoo.com>
2018-11-22 13:39:59 -0600

    Add project files.

---
+++ CKY Algorithm/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CKY_Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader streamReader = new StreamReader("input.txt");
+            string line = streamReader.ReadLine();
+            Debug.WriteLine("my boyfren?");
+        }
+    }
+}
